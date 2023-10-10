d2858b2de21277b18d9dcfb2b50da035a5645813
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2023-10-10 14:47:34 -0300

    segundo dia

--- src/main/java/br/com/joseduarte/todolist/user/UserController.java
+++ src/main/java/br/com/joseduarte/todolist/user/UserController.java
@@ -1,15 +1,28 @@
 package br.com.joseduarte.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel user){
-    System.out.println(user.name);
-}
+    public ResponseEntity<?> create(@RequestBody UserModel user) {
+        if (!Objects.isNull(this.userRepository.findByUsername(user.getUsername())))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already registered");
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.userRepository.save(user));
+    }
+
+
 }
